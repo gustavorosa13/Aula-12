2f829691bc03a3f77af77804cc5757c96462c907
Gustavo Gonçalves da Rosa <142434921+gustavorosa13@users.noreply.github.com>
2023-08-31 16:45:58 -0300

    Create exercicios.js

---
+++ exercicios.js
@@ -0,0 +1,106 @@
+// this is a js file
+// Gustavo Gonçalves da Rosa
+
+/*function minhaFuncao(variavel) {
+    return variavel * 5
+    }
+    console.log(minhaFuncao(2)) // ele está representando contas 5x2 e 5x10                   ele não representaria no console
+    console.log(minhaFuncao(10))
+
+    let textoDoUsuario = prompt("Insira um texto");
+const outraFuncao = function(texto) {
+return texto.toLowerCase().includes("cenoura") //ele retorna um texto que verifica se existe a palavra "cenoura", a ordem de saída seria, i=true, ii=false, iii=false
+}
+const resposta = outraFuncao(textoDoUsuario)
+console.log(resposta)*/
+
+//----------------------------------------------------------------------------------------------------- Exercícios de Código
+
+// "Eu sou Caio, tenho 23 anos, moro em São Paulo e sou estudante."
+
+/*function eu(nome, idade, endereço, profissão) {
+    const eu2 = ("Meu nome é " + nome + " eu tenho " + idade + " anos " + " eu moro em " + endereço + " minha ocupação é " + profissão)
+    return eu2
+}
+
+const pessoal = eu("Gustavo", 17, "SãoLeopoldo", "Estudante");
+console.log(pessoal);
+
+//------------------------------------------------------------------------------------------------------
+// A
+function calculo(primeiro, segundo) {
+    return (primeiro + segundo)
+       
+}
+
+const resultado = calculo(1, 2);
+console.log(resultado);*/
+
+//--------------------------------------------------------------------------------------------------------
+// B
+/*function calculo(primeiro, segundo) {
+    return (primeiro >= segundo)
+}
+
+const resultado = calculo(1, 2);
+console.log(resultado);*/
+
+//------------------------------------------------------------------------------------------------------------
+//C
+
+
+
+/*function calculo(primeiro) {
+    const calculo2 = (primeiro % 2 === 0)
+    return calculo2
+}
+
+let primeiro = prompt("fale um número");
+
+const resultado = calculo(primeiro);
+console.log(resultado);*/
+
+//------------------------------------------------------------------------------------------------------------
+//D
+
+/*function frase(texto) {
+    return (texto.length + " " + texto.toUpperCase())
+
+}
+
+let texto = prompt("Escreva uma frase");
+const resultado = frase(texto);
+console.log(resultado);*/
+
+//-----------------------------------------------------------------------------------------------------------
+
+function soma (a, b) {
+    return (a+b)
+}
+
+function subtração (a, b) {
+    return (a-b)
+}
+
+function multiplicacao (a, b) {
+    return (a*b)
+}
+
+function divisao (a, b) {
+    return (a/b)
+}
+
+//const resultado1 = soma(2, 4);
+//const resultado2 = subtração(2, 4);
+//const resultado3 = multiplicacao(2, 4);                 // Alternativa 1
+//const resultado4 = divisao(2, 4);
+
+
+//let numero1 = prompt("fale um número");
+//let numero2 = prompt("fale um número");                // Alternativa 2
+//const numeros = (soma(numero1, numero2) + " " + subtração(numero1, numero2) + " " + multiplicacao(numero1, numero2) + " " + divisao(numero1, numero2));     
+
+
+//console.log(resultado1, resultado2, resultado3, resultado4); Alternativa 1
+
+//console.log(numeros); Alternativa 2
